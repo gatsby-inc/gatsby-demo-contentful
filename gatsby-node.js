0ccc2fbc1d74df5e45f8d02751d6ef10790fc32f
pauliescanlon <pauliescanlon@gmail.com>
2021-10-14 14:04:38 +0100

    feat: switch posts to createPages

---
+++ gatsby-node.js
@@ -0,0 +1,40 @@
+const path = require('path')
+
+exports.createPages = async ({
+  graphql,
+  actions: { createPage },
+  reporter,
+}) => {
+  const result = await graphql(
+    `
+      {
+        allContentfulPosts {
+          nodes {
+            id
+            url
+          }
+        }
+      }
+    `
+  )
+
+  if (result.errors) {
+    reporter.panicOnBuild(`Error while running GraphQL query.`)
+    return
+  }
+
+  const PostTemplate = path.resolve('./src/templates/post-template.js')
+  result.data.allContentfulPosts.nodes.forEach((node, index) => {
+    const { id, url } = node
+
+    createPage({
+      path: url,
+      component: PostTemplate,
+      context: {
+        id: id,
+        pagePath: url,
+      },
+      defer: index + 1 > 10,
+    })
+  })
+}
